6eafa6d373b923b45486def64cff0aff89349bac
Yamini Chitikela <chitikelay1@montclair.edu>
2023-01-30 18:09:52 -0500

    adding new files

---
+++ StackSample.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class StackSample {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = 0; i < 10; i++){
+            stack.push(i);
+        }
+        System.out.println("Stack: " + stack.toString());
+
+        Integer i;
+        while((i = stack.pop()) != null){
+            System.out.println("Retrieved " + i);
+        }
+        System.out.println("Stack is empty");
+    }
+} 
+    
+
