d533f4aa75b6944339f809be9ad70f10c5121da9
yamini-chitikela <123372324+yamini-chitikela@users.noreply.github.com>
2023-04-03 18:35:32 -0400

    Add files via upload

---
+++ module6/Jlabels/OptionPaneExample.java
@@ -0,0 +1,12 @@
+package module6.Jlabels;
+import javax.swing.*;  
+public class OptionPaneExample {  
+JFrame f;  
+OptionPaneExample(){  
+    f=new JFrame();  
+    JOptionPane.showMessageDialog(f,"Hello, Welcome to Javatpoint.");  
+}  
+public static void main(String[] args) {  
+    new OptionPaneExample();  
+}  
+}  
